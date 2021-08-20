b897c3921905401c7aa279112f41ae28a32d74df
Peder Stray <peder.stray@gmail.com>
2021-08-20 14:33:20 +0200

    Updated contact info, copyright plus cleanup

--- query.pl
+++ query.pl
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2001-2019 by Peder Stray <peder@ninja.no>
+# Copyright (C) 2001-2021 by Peder Stray <peder.stray@gmail.com>
 #
 
 use strict;
@@ -11,12 +11,12 @@ use POSIX;
 #use Data::Dumper;
 
 use vars qw{$VERSION %IRSSI};
-($VERSION) = '$Revision: 1.25 $' =~ / (\d+\.\d+) /;
+($VERSION) = '$Revision: 1.26 $' =~ / (\d+\.\d+) /;
 %IRSSI = (
 	  name	      => 'query',
 	  authors     => 'Peder Stray',
-	  contact     => 'peder@ninja.no',
-	  url	      => 'http://ninja.no/irssi/query.pl',
+	  contact     => 'peder.stray@gmail.com',
+	  url	      => 'https://github.com/pstray/irssi-query',
 	  license     => 'GPL',
 	  description => 'Give you more control over when to jump to query windows and when to just tell you one has been created. Enhanced autoclose.',
 	 );
@@ -33,7 +33,7 @@ sub load_defaults {
     local *FILE;
 
     %defaults = ();
-    open FILE, "< $file";
+    open FILE, "<", $file;
     while (<FILE>) {
 	my($mask,$maxage,$immortal) = split;
 	$defaults{$mask}{maxage}   = $maxage;
@@ -46,7 +46,7 @@ sub save_defaults {
     my $file = Irssi::get_irssi_dir."/query";
     local *FILE;
 
-    open FILE, "> $file";
+    open FILE, ">", $file;
     for (keys %defaults) {
 	my $d = $defaults{$_};
 	print FILE join("\t", $_,
@@ -224,7 +224,6 @@ sub sig_query_created {
 
     $state{$tag}{$nick} = { time => time };
 
-    # So XMPP works too...
     if (ref($serv) eq 'Irssi::Irc::Server') {
 	$serv->redirect_event('userhost', 1, ":$nick", -1, undef,
 			      {
@@ -232,9 +231,6 @@ sub sig_query_created {
 			       "" => "event empty",
 			      });
 	$serv->send_raw("USERHOST :$nick");
-    }
-    elsif (ref($serv) eq 'Irssi::Xmpp::Server') {
-	# maybe do someting here?
     }
 }
 
@@ -263,7 +259,7 @@ sub sig_redir_query_userhost {
 }
 
 sub sig_session_restore {
-    open STATE, sprintf "< %s/query.state", Irssi::get_irssi_dir;
+    open STATE, "<", sprintf "%s/query.state", Irssi::get_irssi_dir;
     %state = ();	# only needed if bound as command
     while (<STATE>) {
 	chomp;
@@ -276,7 +272,7 @@ sub sig_session_restore {
 }
 
 sub sig_session_save {
-    open STATE, sprintf "> %s/query.state", Irssi::get_irssi_dir;
+    open STATE, ">", sprintf "%s/query.state", Irssi::get_irssi_dir;
     for my $tag (keys %state) {
 	for my $nick (keys %{$state{$tag}}) {
 	    print STATE join("\t",$tag,$nick,%{$state{$tag}{$nick}}), "\n";
